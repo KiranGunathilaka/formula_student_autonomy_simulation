f0f5ae94529f2eaff21fa185dbeb9c2d08ef69c7
Kavija Karunarathna <karunarathnakavija@gmail.com>
2026-03-09 21:27:11 +0530

    YOLO is running and is now capable of detecting cones

---
+++ falcon_ws/src/falcon_cone_perception/launch/camera_cone_detection.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    falcon_pkg = FindPackageShare("falcon_cone_perception")
+    yolo_bringup_pkg = FindPackageShare("yolo_bringup")
+
+    model_path = PathJoinSubstitution([
+        falcon_pkg,
+        "models",
+        "fsoco_yolo11n_best.pt"
+    ])
+
+    yolo_launch = PathJoinSubstitution([
+        yolo_bringup_pkg,
+        "launch",
+        "yolov11.launch.py"
+    ])
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(yolo_launch),
+            launch_arguments={
+                "model": model_path,
+                "input_image_topic": "/zed/image_raw",
+                "device": "cuda:0",
+                "threshold": "0.25",
+                "iou": "0.45",
+                "imgsz_height": "640",
+                "imgsz_width": "640",
+                "use_tracking": "False",
+                "use_3d": "False",
+                "use_debug": "True",
+            }.items(),
+        )
+    ])
